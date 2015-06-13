5e153664d32fad5a2664e80e3b04ef7bab6c29af
Viktor Maier <vikma89@googlemail.com>
2015-06-13 16:15:58 +0200

    Extended KnapPackSolver
    ctor and readInput()

--- main/KnapSackSolver.cpp
+++ main/KnapSackSolver.cpp
@@ -7,13 +7,32 @@
 
 #include "KnapSackSolver.h"
 
-KnapSackSolver::KnapSackSolver() {
-	// TODO Auto-generated constructor stub
-
+KnapSackSolver::KnapSackSolver(std::string inputFilename, std::string outputFilename, int nrOfExecutions)
+: knapSack(0,0,0),
+  inputFilename(inputFilename),
+  outputFilename(outputFilename),
+  numberOfExecutions(nrOfExecutions),
+  itemsOfSolution()
+{
+	initKnapSack();
 }
 
 KnapSackSolver::~KnapSackSolver() {
 	// TODO Auto-generated destructor stub
+}
+
+void KnapSackSolver::initKnapSack() {
+	readInput();
+}
+
+void KnapSackSolver::readInput() {
+	// TODO: maybe we should use a pointer otherwise we have to copy it here.
+	knapSack = KnapSackReader::readKnapSackFrom(inputFilename.c_str());
+}
+
+void KnapSackSolver::writeSolution() const {
+	// TODO Auto-generated stub
+
 }
 
 void KnapSackSolver::setUp() {
--- main/KnapSackSolver.h
+++ main/KnapSackSolver.h
@@ -8,12 +8,17 @@
 #ifndef MAIN_KNAPSACKSOLVER_H_
 #define MAIN_KNAPSACKSOLVER_H_
 
+#include <string>
+
 #include "main/KnapSack.h"
 
 /**
  * The KnapSackSolver is the main class for solving the knapsack problem.
  * It is an abstract class from which each algorithm should extend and
  * implement the abstract method solve().
+ *
+ * It takes care of reading the input data and writing the solution. Further
+ * it collects and writes statistics about executions of solve().
  *
  * setUp(): The abstract function setUp() can be used to transform the standard
  * data format (see class Knapsack) in a specific format for the algorithm.
@@ -26,7 +31,7 @@
  */
 class KnapSackSolver {
 public:
-	KnapSackSolver();
+	KnapSackSolver(std::string inputFilename, std::string outputFilename, int nrOfExecutions=1);
 	virtual ~KnapSackSolver();
 
 	/**
@@ -45,6 +50,21 @@ public:
 	 * Abstract function to solve the knapsack problem. This function will be time measured.
 	 */
 	virtual void solve() = 0;
+
+	/**
+	 * The data on which the derived classes can operate. The knapSack should not be changed.
+	 */
+	KnapSack knapSack;
+
+private:
+	void initKnapSack();
+	void readInput();
+	void writeSolution() const;
+
+
+	const std::string inputFilename;
+	const std::string outputFilename;
+	int numberOfExecutions;
 };
 
 #endif /* MAIN_KNAPSACKSOLVER_H_ */
