24a0640fb23ebd7d1f9e9a5cbb4a87eba6d0c0e6
phineliner <phineliner@gmx.net>
2015-06-26 13:00:02 +0200

    fixed, refactored and commented dynamicprogrammingsolver

--- main/algorithms/DynamicProgrammingSolver.cpp
+++ main/algorithms/DynamicProgrammingSolver.cpp
@@ -1,87 +1,116 @@
 #include "main/algorithms/DynamicProgrammingSolver.h"
-#include <cmath>
 
 DynamicProgrammingSolver::DynamicProgrammingSolver(std::string inputFilename, std::string outputFilename, int nrOfExecutions)
-: KnapSackSolver(inputFilename, outputFilename, nrOfExecutions), rows(knapSack.getNumOfItems() + 1), cols(knapSack.getCapacity() + 1) // +1 for zero row and zero col
+: KnapSackSolver(inputFilename, outputFilename, nrOfExecutions),
+  //init rows and columns with +1 for zero row and zero column
+  itemRows(knapSack.getNumOfItems() + 1), weightColumns(knapSack.getCapacity() + 1)
 {
-	// init result table with zeros
-	table = new double*[rows];
-	for(int i=0; i<rows; i++){
-		table[i] = new double[cols];
-		for(int j=0; j<cols; j++)
-			table[i][j] = 0.0;
+	// initialize result table structure.
+	// each row represents the number of items available for the specific sub problem
+	// each column represents the max capacity of the knapsack for the specific sub problem
+	table = new int*[itemRows];
+	for(int i=0; i<itemRows; i++){
+		table[i] = new int[weightColumns];
+		for(int j=0; j<weightColumns; j++)
+			table[i][j] = 0;
 	}
 }
 
 DynamicProgrammingSolver::~DynamicProgrammingSolver(){
+	// delete allocated table arrays
 	for(int i=0; i<knapSack.getNumOfItems(); i++)
 		delete[] table[i];
 	delete[] table;
 }
 
 void DynamicProgrammingSolver::setUp(){
-
+	// nothing to do here. initialization of table happens in constructor
 }
 
+/**
+ * Solves every subproblem of the given knapsack problem including the knapsack problem itself.
+ * Each entry of the table represents the best worth achievable by the specific sub problem.
+ * The problems are being solved in a bottom-up manner.
+ * We start by solving the lowest subproblem [1,1] and end up solving the main problem at [numOfItems, maxCapacity].
+ *
+ * Why don't we start at [0,0] ? Because worths of row[0] and worths of column[0] stay 0.
+ * This is because a knapsack with 0 capacity can not have any items and a knapsack with 0 items can not weigh anything (0 capacity = 0 items).
+ * Thus the best worths of our base-problems (the sub problems at [i,0] for all [0 <= i < cols] and the sub problems at [0, c] for all [0 <= c < rows])
+ * stay 0.
+ */
 void DynamicProgrammingSolver::solve() {
 	KnapSackItem* items = knapSack.getItems();
 
-	// fill result table. we start at [1,1] because values of row zero and values of column zero stay 0.0 (0 capacity = 0 items)
-	for(int i=1; i<rows; i++){
+	for(int i=1; i < itemRows; i++){
 		//items index must be i-1 because index 0 represents row 1, index 1 represents row 2 etc...
 		int itemsIndex = i-1;
-		for(int j=1; j<cols; j++){
-			if(j < items[itemsIndex].weight){ //can not pick item
-				table[i,j] = table[i-1, j];
+
+		for(int c=1; c < weightColumns; c++){
+			if(c < items[itemsIndex].weight){ //can not pick item
+				table[i][c] = table[i-1][c];
 			}else{ //can pick item. choose if we should pick or not
-				double worthOfNotUsingItem = table[i-1][j];
-				double worthOfUsingItem = items[itemsIndex].worth + table[i-1][j-(int)items[itemsIndex].weight];
-				table[i][j] = worthOfNotUsingItem < worthOfUsingItem ? worthOfUsingItem : worthOfNotUsingItem;
+				int worthOfNotUsingItem = table[i-1][c];
+				int worthOfUsingItem = (int)items[itemsIndex].worth + table[i-1][c-(int)items[itemsIndex].weight];
+				table[i][c] = worthOfNotUsingItem < worthOfUsingItem ? worthOfUsingItem : worthOfNotUsingItem;
 			}
 		}
 	}
 
-	//print table
-	printTable();
-
-	// determine included items
-	int n = rows-1;
-	int c = cols-1;
-	while(c > 0){
-		const double a = table[n][c];
-		const double b = table[n-1][c];
-
-		// item is included
-		if(!MyMath::almostEqual(a, b)){
-
-			itemsOfSolution.insert(itemsOfSolution.end(), items[n]);
-			//move column left
-			c-=items[n-1].weight;
-		}
-		// else item not included in optimal solution
-
-		//move row up
-		n--;
-	}
+	//printTable();
 }
 
+/**
+ * Determines items included in the optimal solution by
+ * traversing the populated result table.
+ * This happens top-down, meaning we start at the most lower right table entry.
+ * This entry contains the optimal worths for the main knapsack problem.
+ * By comparing the worths of specific sub problems we can determine if a
+ * specific item is part of the optimal solution or not.
+ *
+ * If the optimal worths of entries of two rows
+ * table[currItem][currWeight] and table[currItem-1][currWeight]
+ * differ from each other, the item of the current row must be part of the solution.
+ * We add the item and decrease the capacity by the weight of that item.
+ * Thus, new sub problems with the new capacity will be checked until we checked all items.
+ */
 void DynamicProgrammingSolver::tearDown(){
-	for(int i=0; i<itemsOfSolution.size(); i++){
-		std::printf("item: %s\n", itemsOfSolution[i].name->c_str());
+	int currItem = knapSack.getNumOfItems();
+	int currCapacity = knapSack.getCapacity();
+	KnapSackItem* items = knapSack.getItems();
+
+	while(currItem > 0){
+		// compare optimal worths of current sub problems and add item if they differ
+		const int a = table[currItem][currCapacity];
+		const int b = table[currItem-1][currCapacity];
+		if(a!=b){
+			// items index must be currItem-1 because index 0 represents row 1, index 1 represents row 2 etc...
+			int itemsIndex = currItem-1;
+
+			// add item to solution and move item.weight columns to the left since the added item decreases capacity
+			itemsOfSolution.insert(itemsOfSolution.end(), items[itemsIndex]);
+			currCapacity -= (int)items[itemsIndex].weight;
+		}
+
+		//move one row up to the next item
+		currItem--;
 	}
 }
 
 void DynamicProgrammingSolver::printTable(){
+	// +1 for zero row and zero column
+	const int rows = knapSack.getNumOfItems() + 1;
+	const int cols = knapSack.getCapacity() + 1;
+
 	std::printf(" \t|");
 	for(int j=0; j<cols; j++){
-		std::printf("[w:%d]\t|", j);
+		std::printf("[c:%d]\t|", j);
 	}
-	std:printf("\n------------------------------------------------------------------------------------------------------------------\n");
+	std:printf("\n----------------------------------------------------\n");
 	for(int i=0; i<rows; i++){
 			std::printf("[i:%d]\t| ", i);
 			for(int j=0; j<cols; j++){
-				std::printf("%.1f\t| ", table[i][j]);
+				std::printf("%d\t| ", table[i][j]);
 			}
-			std::printf("\n------------------------------------------------------------------------------------------------------------------\n");
+			std::printf("\n----------------------------------------------------\n");
 	}
 }
--- main/algorithms/DynamicProgrammingSolver.h
+++ main/algorithms/DynamicProgrammingSolver.h
@@ -12,6 +12,11 @@
  * implements the abstract method solve().
  *
  * Solves the KnapSack-Problem with the dynamic programming approach.
+ * The main knapsack-problem is being solved in a bottom-up manner by solving
+ * all sub-problems first. When the optimal worths of the base problems
+ * are known, the worths of higher sub problems are calculated until
+ * the worth of main problem is known. Afterwards the items belonging to the
+ * calculated worth are added to the solutionlist.
  */
 class DynamicProgrammingSolver : public KnapSackSolver {
 public:
@@ -29,16 +34,41 @@ protected:
 private:
 
 	/**
-	 * number of rows. each row represents the number of items available for the specific sub problem.
-	 */
-	const int rows;
+	 * The result table.
+	 * Each row represents the number of items available for the specific sub problem
+	 * Each column represents the max capacity of the knapsack for the specific sub problem
+	 *
+	 * The upper left entry represents the optimal worth for the sub-problem [0,0]
+	 * (a knapsack that can choose among 0 items and has a capacity of 0).
+	 *
+	 * The lower right entry represents the optimal worth for the main-problem
+	 * (the knapsack that can choose among all available items and has the highest capacity possible).
+	 *
+	 * Example table:
 
-	/**
-	 * number of colums. each column represents the max capacity of the knapsack for the specific sub problem.
+			  |[c:0]|[c:1]|[c:2]|[c:3]|[c:4]|[c:5]
+		 -----------------------------------------
+		 [i:0]| 0	| 0	  | 0	| 0	  | 0	| 0
+		 -----------------------------------------
+		 [i:1]| 0	| 0	  | 3	| 3	  | 3	| 3
+		 -----------------------------------------
+		 [i:2]| 0	| 0	  | 3	| 4	  | 4	| 7
+		 -----------------------------------------
+		 [i:3]| 0	| 0	  | 3	| 4	  | 5	| 7
+		 -----------------------------------------
+		 [i:4]| 0	| 0	  | 3	| 4	  | 5	| 7
+		 -----------------------------------------
+	 *
 	 */
-	const int cols;
-	double** table;
+	int** table;
+
+	/**each row represents the number of items available for the specific sub problem*/
+	const int itemRows;
+
+	/**each column represents the max capacity of the knapsack for the specific sub problem*/
+	const int weightColumns;
 	
+	/**prints the table to the console*/
 	void printTable();
 
 };
