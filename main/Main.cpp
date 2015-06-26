4102859a103fad96b36edebc28983f5c2ee9b861
phineliner <phineliner@gmx.net>
2015-06-26 12:59:32 +0200

    added a very simple knapsack input file for testing the setup of the dynamic programming table.

--- main/Main.cpp
+++ main/Main.cpp
@@ -1,10 +1,13 @@
 #include "main/algorithms/BruteForceSolver.h"
 #include "main/algorithms/DynamicProgrammingSolver.h"
+#include "main/algorithms/NemhauserUllmannSolver.h"
+#include "main/algorithms/NemhauserUllmannParallelSolver.h"
 using namespace std;
 
 static const char* FIRST_KNAPSACK_INPUT_FILE = "res/firstFileExample.txt";
 static const char* SECOND_KNAPSACK_INPUT_FILE = "res/secondFileExample.txt";
 static const char* THIRD_KNAPSACK_INPUT_FILE = "res/thirdFileExample.txt";
+static const char* DP_INPUT_FILE = "res/dynamicProgrammingExample.txt";
 static const char* KNAPSACK_OUTPUT_FILE = "knapSackOut.txt";
 
 int main(int argc, char* argv[]){
@@ -13,9 +16,21 @@ int main(int argc, char* argv[]){
 	BruteForceSolver* bfsolver = new BruteForceSolver(FIRST_KNAPSACK_INPUT_FILE, KNAPSACK_OUTPUT_FILE);
 	bfsolver->start();
 	delete bfsolver;
-*/
+	 */
 
-	DynamicProgrammingSolver* dpsolver = new DynamicProgrammingSolver(FIRST_KNAPSACK_INPUT_FILE, KNAPSACK_OUTPUT_FILE);
+	/*
+	NemhauserUllmannSolver* nusolver = new NemhauserUllmannSolver(THIRD_KNAPSACK_INPUT_FILE, KNAPSACK_OUTPUT_FILE);
+	nusolver->start();
+	delete nusolver;
+	*/
+
+	/*
+	NemhauserUllmannParallelSolver* nupsolver = new NemhauserUllmannParallelSolver(THIRD_KNAPSACK_INPUT_FILE, KNAPSACK_OUTPUT_FILE);
+	nupsolver->start();
+	delete nupsolver;
+	*/
+
+	DynamicProgrammingSolver* dpsolver = new DynamicProgrammingSolver(DP_INPUT_FILE, KNAPSACK_OUTPUT_FILE);
 	dpsolver->start();
 	delete dpsolver;
 }
